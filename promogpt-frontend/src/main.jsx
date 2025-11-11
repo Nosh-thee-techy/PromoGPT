7c1af3119c69ee50fd16ef9c83f0bb1e67f2a3c4
Nosh-thee-techy <blessingswanjiku15@gmail.com>
2025-11-11 19:38:17 +0000

    fix

--- promogpt-frontend/src/main.jsx
+++ promogpt-frontend/src/main.jsx
@@ -1,18 +1,30 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import { AuthProvider } from "./contexts/AuthContext";
+// import AppRouter from "./AppRouter";
+// import "./index.css";
+
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <AppRouter />
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import AppRouter from "./AppRouter";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <AppRouter />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
