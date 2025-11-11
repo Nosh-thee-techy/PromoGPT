97b88d37ff0f8ed9129ddafaa0d01309c8be1206
Nosh-thee-techy <blessingswanjiku15@gmail.com>
2025-11-11 08:20:18 +0000

    fixed

--- promogpt-frontend/src/main.jsx
+++ promogpt-frontend/src/main.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import AppRouter from "./AppRouter";
+import { AuthProvider } from "./contexts/AuthContexts";
+import AppRouter from "./approuter";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
